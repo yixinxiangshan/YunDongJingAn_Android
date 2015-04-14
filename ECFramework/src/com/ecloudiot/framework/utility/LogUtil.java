a6602c3fc027db7eea66a097ce64e4ea7a4c814c
Richard <richard.zhou@intel.com>
2015-04-14 11:06:43 +0800

    open debug log

--- ECFramework/src/com/ecloudiot/framework/utility/LogUtil.java
+++ ECFramework/src/com/ecloudiot/framework/utility/LogUtil.java
@@ -5,9 +5,9 @@ import android.util.Log;
 public class LogUtil {
 
 	public static void d(String tag, String msg) {
-		if (Constants.DEBUG) {
+//		if (Constants.DEBUG) {
 			Log.d(tag, msg);
-		}
+//		}
 	}
 
 	public static void e(String tag, String msg) {
@@ -19,20 +19,20 @@ public class LogUtil {
 	}
 
 	public static void i(String tag, String msg) {
-		if (Constants.DEBUG) {
+//		if (Constants.DEBUG) {
 			Log.i(tag, msg);
-		}
+//		}
 	}
 
 	public static void v(String tag, String msg) {
-		if (Constants.DEBUG) {
+//		if (Constants.DEBUG) {
 			Log.v(tag, msg);
-		}
+//		}
 	}
 
 	public static void w(String tag, String msg) {
-		if (Constants.DEBUG) {
+//		if (Constants.DEBUG) {
 			Log.w(tag, msg);
-		}
+//		}
 	}
 }
